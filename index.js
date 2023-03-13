68e20bd5ae96610b44377d8e892dc0fc4308d079
xITSDUCKYx <82958945+xITSDUCKYx@users.noreply.github.com>
2023-03-12 21:45:54 -0400

    Fixed javascript
    I made the JavaScript better and remove an error causing the JavaScript not to function.

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let incrementBtn = document.getElementById("increment-btn")
+let saveBtn = document.getElementById("save-btn")
 let count = 0
 
-function increment() {
+incrementBtn.addEventListener("click",function(){
     count += 1
     countEl.textContent = count
-}
+})
 
-function save() {
+saveBtn.addEventListener("click",function(){
     let countStr = count + " - "
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
-}
+console.log("clicked save")
+})
+
+  
