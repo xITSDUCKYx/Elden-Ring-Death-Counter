64629843863726733ac48351fb9816ee55797d63
xITSDUCKYx <82958945+xITSDUCKYx@users.noreply.github.com>
2023-03-17 09:10:58 -0400

    p tag change
    Move id in p tag to an added span tag with in the p tag.

--- index.js
+++ index.js
@@ -4,17 +4,39 @@ let incrementBtn = document.getElementById("increment-btn")
 let saveBtn = document.getElementById("save-btn")
 let count = 0
 
-incrementBtn.addEventListener("click",function(){
-    count += 1
-    countEl.textContent = count
-})
+// incrementBtn.addEventListener("click",function(){
+//     count += 1
+//     countEl.textContent = count
+// })
 
-saveBtn.addEventListener("click",function(){
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
-console.log("clicked save")
-})
+// saveBtn.addEventListener("click",function(){
+//     let countStr = count + " - "
+//     saveEl.textContent += countStr
+//     countEl.textContent = 0
+//     count = 0
+// console.log("clicked save")
+// })
 
-  
+
+document.addEventListener("click", function(e){
+ 
+    console.log(e.target)
+    if (e.target == incrementBtn){
+        count++
+        countEl.textContent = count
+    } else if (e.target == saveBtn){
+        let countStr = ""
+        if (saveEl.textContent){
+          countStr += " - " + count
+
+        } else {
+          countStr += count
+        }
+
+        saveEl.textContent += countStr
+        count = 0
+        countEl.textContent = 0 
+    }else {
+        console.log("Clicked on the page")
+    }
+})
